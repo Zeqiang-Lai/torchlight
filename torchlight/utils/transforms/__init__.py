aafd75b95d92f4529344dc40e2f1b4176fddce97
zeqiang-lai <laizeqiang@outlook.com>
2021-09-17 15:09:42 +0800

    expose api

--- torchlight/utils/transforms/__init__.py
+++ torchlight/utils/transforms/__init__.py
@@ -1,3 +1,3 @@
-from .degrade import GaussianBlur, UniformBlur, KFoldDownsample, UniformDownsample, GaussianDownsample, HSI2RGB
+from .degrade import GaussianBlur, UniformBlur, KFoldDownsample, UniformDownsample, GaussianDownsample, HSI2RGB, Upsample
 from .noise import GaussianNoise, GaussianNoiseBlind, GaussianNoiseBlindv2, ImpulseNoise, StripeNoise, DeadlineNoise, MixedNoise
 from .general import Compose, MinMaxNormalize, CenterCrop, RandCrop
