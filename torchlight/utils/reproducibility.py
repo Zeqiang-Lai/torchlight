532ec9d2dea58efef5ec06b4a9ad54a5d5655b59
laizeqiang <laizeqiang@outlook.com>
2021-09-29 14:26:52 +0800

    fix SeedDataloader

--- torchlight/utils/reproducibility.py
+++ torchlight/utils/reproducibility.py
@@ -1,10 +1,15 @@
-import torch
+from typing import Sequence, Optional
 import random
-import numpy as np
+
+import torch
 from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import Dataset, Sampler
+import numpy as np
+
 
 # This module provide utils for controling reproducibility
 # https://pytorch.org/docs/stable/notes/randomness.html
+
 
 def setup_randomness(seed=1234, deterministic=True):
     torch.manual_seed(seed)
@@ -14,6 +19,7 @@ def setup_randomness(seed=1234, deterministic=True):
     if deterministic:
         torch.backends.cudnn.benchmark = False
         torch.use_deterministic_algorithms(True)
+
 
 def seed_worker(worker_id):
     worker_seed = torch.initial_seed() % 2**32
@@ -26,9 +32,35 @@ def seed_generator(seed):
     g.manual_seed(seed)
 
 
-def SeedDataLoader(*args, seed=0, **kwargs):
-    return DataLoader(*args,
-        worker_init_fn=seed_worker,
-        generator=seed_generator(seed),
-        **kwargs
-    )
+class SeedDataLoader(DataLoader):
+    def __init__(self,
+                 dataset: Dataset,
+                 seed: Optional[int] = 0,
+                 batch_size: Optional[int] = 1,
+                 shuffle: bool = False,
+                 sampler: Optional[Sampler[int]] = None,
+                 batch_sampler: Optional[Sampler[Sequence[int]]] = None,
+                 num_workers: int = 0,
+                 collate_fn = None,
+                 pin_memory: bool = False,
+                 drop_last: bool = False,
+                 timeout: float = 0,
+                 multiprocessing_context=None, *,
+                 prefetch_factor: int = 2,
+                 persistent_workers: bool = False):
+
+        super().__init__(dataset,
+                         batch_size=batch_size,
+                         shuffle=shuffle,
+                         sampler=sampler,
+                         batch_sampler=batch_sampler,
+                         num_workers=num_workers,
+                         collate_fn=collate_fn,
+                         pin_memory=pin_memory,
+                         drop_last=drop_last,
+                         timeout=timeout,
+                         worker_init_fn=seed_worker,  # use seed worker
+                         multiprocessing_context=multiprocessing_context,
+                         generator=seed_generator(seed),  # use seed generator
+                         prefetch_factor=prefetch_factor,
+                         persistent_workers=persistent_workers)
