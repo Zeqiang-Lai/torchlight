ff06252a695148685711b94f892d261485e0222e
laizeqiang <laizeqiang@outlook.com>
2021-10-09 12:10:33 +0800

    add graident && warp operations

---
+++ torchlight/nn/ops/warp.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn.functional as F
+
+
+def warp(x, flo):
+    """
+    warp an image/tensor (im2) back to im1, according to the optical flow
+    x: [B, C, H, W] (im2)
+    flo: [B, 2, H, W] flow
+    """
+    B, C, H, W = x.size()
+    # mesh grid
+    xx = torch.arange(0, W).view(1, -1).repeat(H, 1)
+    yy = torch.arange(0, H).view(-1, 1).repeat(1, W)
+    xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    grid = torch.cat((xx, yy), 1).float()
+    
+    if x.is_cuda:
+        grid = grid.cuda()
+    vgrid = grid + flo
+    # scale grid to [-1,1]
+    vgrid[:, 0, :, :] = 2.0 * vgrid[:, 0, :, :].clone() / max(W - 1, 1) - 1.0
+    vgrid[:, 1, :, :] = 2.0 * vgrid[:, 1, :, :].clone() / max(H - 1, 1) - 1.0
+
+    vgrid = vgrid.permute(0, 2, 3, 1)
+    output = F.grid_sample(x, vgrid)
+    mask = torch.ones(x.size()).to(x.device)
+    mask = F.grid_sample(mask, vgrid)
+
+    mask[mask < 0.999] = 0
+    mask[mask > 0] = 1
+
+    return output
