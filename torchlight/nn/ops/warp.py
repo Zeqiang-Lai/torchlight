a602e395885610399bef81ccbb6cf5139731956d
zeqiang-lai <laizeqiang@outlook.com>
2021-10-18 19:26:53 +0800

    add serveral ops and flow utils

--- torchlight/nn/ops/warp.py
+++ torchlight/nn/ops/warp.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn.functional as F
 
 
-def warp(x, flo):
+def flow_warp(x, flow, **kwargs):
     """
     warp an image/tensor (im2) back to im1, according to the optical flow
     x: [B, C, H, W] (im2)
-    flo: [B, 2, H, W] flow
+    flow: [B, 2, H, W] flow
+    kwargs: kwargs of grid_sample
     """
     B, C, H, W = x.size()
     # mesh grid
@@ -15,20 +16,16 @@ def warp(x, flo):
     xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
     yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
     grid = torch.cat((xx, yy), 1).float()
-    
+
     if x.is_cuda:
         grid = grid.cuda()
-    vgrid = grid + flo
+    vgrid = grid + flow
+    
     # scale grid to [-1,1]
     vgrid[:, 0, :, :] = 2.0 * vgrid[:, 0, :, :].clone() / max(W - 1, 1) - 1.0
     vgrid[:, 1, :, :] = 2.0 * vgrid[:, 1, :, :].clone() / max(H - 1, 1) - 1.0
 
     vgrid = vgrid.permute(0, 2, 3, 1)
-    output = F.grid_sample(x, vgrid)
-    mask = torch.ones(x.size()).to(x.device)
-    mask = F.grid_sample(mask, vgrid)
-
-    mask[mask < 0.999] = 0
-    mask[mask > 0] = 1
+    output = F.grid_sample(x, vgrid, **kwargs)
 
     return output
