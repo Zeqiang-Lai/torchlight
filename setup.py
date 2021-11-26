34ccb0cbf0651e163394e4b8227726f5d2c7adf0
zeqiang-lai <laizeqiang@outlook.com>
2021-11-26 22:15:05 +0800

    bug fixs: release to pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='torchlights',
     packages=find_packages(),
-    version='0.3.0',
-    install_requires=['munch', 'colorama', 'readchar', 'tqdm', 'qqdm', 'pyyaml', 'colorlog']
+    version='0.3.2',
+    install_requires=['munch', 'colorama', 'readchar', 'tqdm', 'qqdm', 'pyyaml', 'colorlog'],
+    include_package_data=True
 )
