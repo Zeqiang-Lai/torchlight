1d9dd9068e1334a0c1181baba635b41ac9dc4f18
zeqiang-lai <laizeqiang@outlook.com>
2021-11-29 21:27:26 +0800

    just file structure changes

--- examples/demo/test_config.py
+++
@@ -1,7 +0,0 @@
-import json
-from torchlight.utils.config import ConfigParser
-
-if __name__ == '__main__':
-    # ConfigParser.backend(json, ext='json')
-    config = ConfigParser.default("test")
-    print(config['trainer'])
