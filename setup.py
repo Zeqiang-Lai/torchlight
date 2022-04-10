5ff45dff417c926e89945baf68549b98c6c94a0b
zeqiang-lai <laizeqiang@outlook.com>
2022-04-10 16:04:42 +0800

    release 0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='torchlights',
     packages=find_packages(),
-    version='0.3.2',
+    version='0.3.3',
     install_requires=['munch', 'colorama', 'readchar', 'tqdm', 'qqdm', 'pyyaml', 'colorlog'],
     include_package_data=True
 )
