79140325f65bdda385b1a825b3dfd87162e80410
laizeqiang <laizeqiang@outlook.com>
2022-06-03 20:19:10 +0800

    fix SIDD

--- torchlight/data/datasets/common/SIDD.py
+++ torchlight/data/datasets/common/SIDD.py
@@ -3,17 +3,17 @@ from os.path import join
 from ..pair import DatasetTrain, DatasetVal
 
 
-class TrainDataSet(DatasetTrain):
+class Train(DatasetTrain):
     def __init__(self, basedir, patch_size):
         basedir = join(basedir, 'train')
-        imgA_dir = join(basedir, 'input'),
-        imgB_dir = join(basedir, 'groundtruth'),
+        imgA_dir = join(basedir, 'input')
+        imgB_dir = join(basedir, 'groundtruth')
         super().__init__(imgA_dir, imgB_dir, patch_size)
 
 
-class ValDataSet(DatasetVal):
-    def __init__(self, basedir, patch_size):
+class Valid(DatasetVal):
+    def __init__(self, basedir, patch_size=None):
         basedir = join(basedir, 'valid')
-        imgA_dir = join(basedir, 'input'),
-        imgB_dir = join(basedir, 'target'),
+        imgA_dir = join(basedir, 'input')
+        imgB_dir = join(basedir, 'target')
         super().__init__(imgA_dir, imgB_dir, patch_size)
