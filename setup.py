558ab3c3a27361e68ee3a46d41bfc23ff34d7aac
zeqiang-lai <laizeqiang@outlook.com>
2022-06-11 21:37:56 +0800

    release 0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='torchlights',
     packages=find_packages(),
-    version='0.3.4',
+    version='0.3.5',
+    description='Torchlights - an light-weight PyTorch trainer, as well as many useful utils',
+    author='Zeqiang Lai',
+    author_email='laizeqiang@outlook.com',
+    long_description_content_type = 'text/markdown',
+    url='https://github.com/Zeqiang-Lai/torchlight',
     install_requires=['munch', 'colorama', 'readchar', 'tqdm', 'qqdm', 'pyyaml', 'colorlog'],
     include_package_data=True
 )
