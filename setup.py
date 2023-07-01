575d4fc78375a7320ed234504fd079719b126195
Zeqiang-Lai <916323301@qq.com>
2023-07-02 00:30:08 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='torchlights',
     packages=find_packages(),
-    version='0.3.8',
+    version='0.4.0',
     description='Torchlights - an light-weight PyTorch trainer, as well as many useful utils',
     author='Zeqiang Lai',
     author_email='laizeqiang@outlook.com',
